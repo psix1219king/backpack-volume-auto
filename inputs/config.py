1e2ada40c39165ce9828227992597d81acb75523
psix1219 <142375702+psix1219king@users.noreply.github.com>
2024-03-26 11:20:04 +0700

    Update config.py

--- inputs/config.py
+++ inputs/config.py
@@ -1,6 +1,6 @@
 THREADS = 1  # Enter amount of threads
 DELAY_BETWEEN_TRADE = (1, 2)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
-DELAY_BETWEEN_DEAL = (0, 0)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
+DELAY_BETWEEN_DEAL = (10, 15)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
 
 NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0  # min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
